939da8fe9b83e4864be04447f6cfe6eb02ad4e27
Ahmad Saif <ahmadsaif@mahmoud-desktop.(none)>
2009-11-19 19:16:43 +0200

    modify mysql checking way to print mysql status

--- src/mysql.sh
+++ src/mysql.sh
@@ -5,8 +5,8 @@
 
 # Test for credentials
 # TODO: Check for existence of databases
-echo SHOW DATABASES | mysql -u$Prj_mysql_user -p$Proj_mysql_pass || exit 1
+`mysql -u"$Prj_mysql_user" -p"$Proj_mysql_pass" -Bse 'status'` || exit 1
 
 for db in $Prj_mysql_dbs;
 	do mysqldump -u$Prj_mysql_user -p$Prj_mysql_pass $db | gzip > $Back_dir/$Suffix.DB-$db.sql.gz && echo $Back_dir/$Suffix.DB-$db.sql.gz;
-done && exit 0
+done
