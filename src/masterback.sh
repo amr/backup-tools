fba809c528ed9ed5dd4631549832a48273aa296c
Amr Mostafa <amr.mostafa@egyptdc.com>
2009-11-19 17:11:05 +0200

    Use correct var for dirs and redirect errors to /dev/null

--- src/masterback.sh
+++ src/masterback.sh
@@ -1,6 +1,6 @@
-#! /bin/sh
+#!/bin/sh
 $Suffix=$Prj_name.`date +%F`
-echo $Prj_name | files.sh $Back_dir/$Suffix
+echo $Prj_dirs | files.sh $Back_dir/$Suffix 2>/dev/null
 sh mysql.sh
 `rsync -avzr -e "ssh -i "Rsync_key"" "$1" "$Back_user"@"$Back_server":"$Remote_dir"`
 exit 0
