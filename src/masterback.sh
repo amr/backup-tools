67438e3ea60bd02c965193a4e84e20f6a5cda97f
Ahmad Saif <ahmadsaif@mahmoud-desktop.(none)>
2009-11-19 20:21:42 +0200

    rsync form the results at the tempfile

--- src/masterback.sh
+++ src/masterback.sh
@@ -8,5 +8,6 @@ echo $Prj_dirs | files.sh $Back_dir/$Suffix 2>/dev/null
 env Prj_mysql_dbs="$Prj_mysql_dbs" Prj_mysql_user="$Prj_mysql_user" Prj_mysql_pass="$Prj_mysql_pass" Suffix="$Suffix" Back_dir="$Back_dir" mysql.sh
 
 # Push
-`rsync -avzr -e "ssh -i "Rsync_key"" "$1" "$Back_user"@"$Back_server":"$Remote_dir"`
+for i in $(cat tempfile);
+rsync -avzr -e ssh $i "$Back_user"@"$Back_server":"$Remote_dir"
 exit 0
