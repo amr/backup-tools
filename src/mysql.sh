bcbce9c4108764f958cdbfb38b0f686a262490c8
Ahmad Saif <ahmadsaif@mahmoud-desktop.(none)>
2009-11-22 10:41:37 +0200

    change the result file name th the project name

--- src/mysql.sh
+++ src/mysql.sh
@@ -8,5 +8,5 @@
 #`mysql -u"$Prj_mysql_user" -p"$Proj_mysql_pass" -Bse 'status'` || exit 1
 
 for db in $Prj_mysql_dbs;
-	do mysqldump -u$Prj_mysql_user -p$Prj_mysql_pass $db | gzip > $Back_dir/$Suffix.DB-$db.sql.gz && echo $Back_dir/$Suffix.DB-$db.sql.gz > tempfile;
+	do mysqldump -u$Prj_mysql_user -p$Prj_mysql_pass $db | gzip > $Back_dir/$Prj_name.DB-$db.sql.gz && echo $Back_dir/$Prj_name.DB-$db.sql.gz >> tempfile;
 done
