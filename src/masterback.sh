493c5199c75b8d0c243ba6b756809330afdcd6e6
Ahmad Saif <ahmadsaif@mahmoud-desktop.(none)>
2009-11-22 10:42:39 +0200

    compress files and DB in one file and rsyinc it

--- src/masterback.sh
+++ src/masterback.sh
@@ -2,12 +2,14 @@
 $Suffix=$Prj_name.`date +%F`
 
 # Files
-echo $Prj_dirs | files.sh $Back_dir/$Suffix 2>/dev/null
+echo $Prj_dirs | ./files.sh $Back_dir/$Prj_name.Files 2>/dev/null
 
 # MySQL
-env Prj_mysql_dbs="$Prj_mysql_dbs" Prj_mysql_user="$Prj_mysql_user" Prj_mysql_pass="$Prj_mysql_pass" Suffix="$Suffix" Back_dir="$Back_dir" mysql.sh
+env Prj_mysql_dbs="$Prj_mysql_dbs" Prj_mysql_user="$Prj_mysql_user" Prj_mysql_pass="$Prj_mysql_pass" Prj_name="$Prj_name" Back_dir="$Back_dir" ./mysql.sh
+
+# Compressing Results
+cd $Back_dir && tar -czf $Suffix.tar.gz $Prj_name* && rm -rf `cat tempfile` tempfile 
 
 # Push
-for i in $(cat tempfile);
-rsync -avzr -e ssh $i "$Back_user"@"$Back_server":"$Remote_dir"
+rsync -avzr -e ssh $Suffix.tar.gz "$Back_user"@"$Back_server":"$Remote_dir"
 exit 0
