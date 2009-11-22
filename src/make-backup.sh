3eed0eebb1c18309e2dea7cd284c38b613957b7a
Amr Mostafa <amr.mostafa@egyptdc.com>
2009-11-22 18:37:39 +0200

    Cleanup make-backup.sh

--- src/make-backup.sh
+++ src/make-backup.sh
@@ -17,37 +17,37 @@ test -a "$1" && source "$1"
 
 ###
 # Unique ID for today's backup
-#
 BACKUP_ID="$PROJECT_NAME.`date +%F.%s`"
 
 ###
 # Log
-#
-LOG="$BACKUP_ID.log"
+LOG="logs/$BACKUP_ID.log"
+touch $LOG
+LOG=$(readlink -f $LOG)
 
 # Print fatal error and exit
 function fatal_error() {
 	echo "[ERROR] $1" >&2
 	echo "[ERROR] $1" >> $LOG
-	mail
+	mail_report "Failed"
 	exit 1;
 }
 
 # Print informational message
 function info() {
-	echo "[INFO] $1"
 	echo "[INFO] $1" >> $LOG
+	echo "[INFO] $1"
 }
 
 # Validate run environment
 function validate() {
 	REQUIRED_VARS=(PROJECT_NAME PROJECT_DIRECTORIES PROJECT_MYSQL_DATABASES MYSQL_USER MYSQL_PASSWORD LOCAL_BACKUP_DIRECTORY REMOTE_BACKUP_HOST REMOTE_BACKUP_USER REMOTE BACKUP_DIRECTORY)
 	for var in ${REQUIRED_VARS[@]}; do
-		test -n ${!$var} || fatal_error "$var is empty"
+		test -n $`echo $var` || fatal_error "$var is empty"
 	done;
 
 	# Project directories
-	for dir in "$PROJECT_DIRECTORIES"; do
+	for dir in $PROJECT_DIRECTORIES; do
 		test -d "$dir" || fatal_error "Invalid directory specified in PROJECT_DIRECTORIES: $dir"
 	done;
 }
@@ -63,43 +63,46 @@ validate
 
 ###
 # Backup files
-echo "$PROJECT_DIRECTORIES" | toolbox/files.sh "$LOCAL_BACKUP_DIRECTORY/$PROJECT_NAME.files" 2>/dev/null
+FILES=$(echo "$PROJECT_DIRECTORIES" | toolbox/files.sh "$LOCAL_BACKUP_DIRECTORY/$PROJECT_NAME.files")
 if [ $? == 0 ]; then 
-	log "Your files have been backed up correctly"
+	info "Your files have been backed up correctly"
 else 
 	fatal_error "Something is wrong please check your Files configuration"
 fi
 
 ###
 # Backup MySQL
-env PROJECT_MYSQL_DATABASES="$PROJECT_MYSQL_DATABASES" MYSQL_USER="$MYSQL_USER" MYSQL_PASSWORD="$MYSQL_PASSWORD" PROJECT_NAME="$PROJECT_NAME" LOCAL_BACKUP_DIRECTORY="$LOCAL_BACKUP_DIRECTORY" toolbox/mysql.sh
+DATABASES=$(env PROJECT_MYSQL_DATABASES="$PROJECT_MYSQL_DATABASES" MYSQL_USER="$MYSQL_USER" MYSQL_PASSWORD="$MYSQL_PASSWORD" PROJECT_NAME="$PROJECT_NAME" LOCAL_BACKUP_DIRECTORY="$LOCAL_BACKUP_DIRECTORY" toolbox/mysql.sh)
 if [ $? == 0 ]; then 
-	log "Your DataBase have been Backed up correctly"
+	info "Your DataBase have been Backed up correctly"
 else 
 	fatal_error "Something is wrong please check your DataBase configuration"
 fi
 
 ###
-# Compressing Results
-#
-cd $LOCAL_BACKUP_DIRECTORY && tar -czf $BACKUP_ID.tar.gz $PROJECT_NAME* && rm -rf `cat tempfile` tempfile 
+# Create backup package
+cd "$LOCAL_BACKUP_DIRECTORY" && mkdir "$BACKUP_ID" && mv $FILES $DATABASES "$BACKUP_ID/"
+if [ $? == 0 ]; then 
+	info "Your project has been backed up correctly"
+else 
+	fatal_error "Something is wrong please contact the backup administrator"
+fi
 
 ###
 # Report backup size
-log "Your backup size is `ls -sh | sort -rn | head -1 | cut -d " " -f1`"
+info "Your backup size is `du -hs $BACKUP_ID | awk '{ print $1 }'`"
 
 ###
 # Push to remote server
-rsync -avzr -e ssh "$BACKUP_ID.tar.gz" "$REMOTE_BACKUP_USER"@"$REMOTE_BACKUP_HOST":"$REMOTE_BACKUP_DIRECTORY"
-
-# Monitor Pushing files
-if [ $? == 0 ]; then 
-	log "Your Backup have been sent correctly to the backup server ($REMOTE_BACKUP_HOST) "
-else 
-	fatal_error "Your backup didn't reach the backup server. Please contact the backup administrator"
-fi
+#rsync -avzr -e ssh "$BACKUP_ID.tar.gz" "$REMOTE_BACKUP_USER"@"$REMOTE_BACKUP_HOST":"$REMOTE_BACKUP_DIRECTORY"
+#if [ $? == 0 ]; then 
+#	info "Your Backup have been sent correctly to the backup server ($REMOTE_BACKUP_HOST) "
+#else 
+#	fatal_error "Your backup didn't reach the backup server. Please contact the backup administrator"
+#fi
 
 # Send report
-mail
+info "Sending backup log to: $PROJECT_OWNERS"
+mail_report "Success"
 
 exit 0
