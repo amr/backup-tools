561716cc7e8a8729768186b854868913f78b9b3d
Amr Mostafa <amr.mostafa@egyptdc.com>
2009-11-23 12:21:41 +0200

    Removed 2>/dev/null redirect from files.sh, we want to see the errors

--- toolbox/files.sh
+++ toolbox/files.sh
@@ -4,4 +4,4 @@
 # Creates a single compressed archive of files, reading paths from stdin.
 #
 
-xargs tar -czf $1.tar.gz 2>/dev/null && echo $1.tar.gz && exit 0
+xargs tar -czf $1.tar.gz && echo $1.tar.gz && exit 0
