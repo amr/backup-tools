2f19624c3bbfe6d7f8bdf380cdfd3b7ac34e23b9
Amr Mostafa <amr.mostafa@egyptdc.com>
2009-12-07 22:28:53 +0200

    Fixed a bug when an empty MySQL password is used

--- toolbox/mysql.sh
+++ toolbox/mysql.sh
@@ -4,6 +4,16 @@
 # Makes a backup copy of a MySQL database
 #
 
+# Prepare mysqldump args, this is needed to include only non-empty arguments.
+MYSQLDUMP_OPTS=""
+if [ -z "$MYSQL_USER" ]; then
+	MYSQLDUMP_OPTS="$MYSQLDUMP_OPTS -u$MYSQL_USER"
+fi;
+if [ -z "$MYSQL_PASSWORD" ]; then
+	MYSQLDUMP_OPTS="$MYSQLDUMP_OPTS -p$MYSQL_PASSWORD"
+fi;
+
+# Execute mysqldump on given databases
 for db in $PROJECT_MYSQL_DATABASES;
-	do mysqldump -u$MYSQL_USER -p$MYSQL_PASSWORD $db | gzip > $LOCAL_BACKUP_DIRECTORY/$PROJECT_NAME.DB-$db.sql.gz && echo $LOCAL_BACKUP_DIRECTORY/$PROJECT_NAME.DB-$db.sql.gz;
+	do mysqldump $MYSQLDUMP_OPTS $db | gzip > $LOCAL_BACKUP_DIRECTORY/$PROJECT_NAME.DB-$db.sql.gz && echo $LOCAL_BACKUP_DIRECTORY/$PROJECT_NAME.DB-$db.sql.gz;
 done
