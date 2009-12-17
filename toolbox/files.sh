55a3acae300c8f2937d8f5e1ce423e29b8e82388
Amr Mostafa <amr.mostafa@egyptdc.com>
2009-12-17 15:22:49 +0200

    Revert "Bug fix (thanks to Omar): Do not store symlinks or hardlinks, but store the files they point to"
    Turns out we don't really want this, imagine you are symlinking a big directory in 5 projects, you don't really want it to be backed up 5 times.
    
    This reverts commit b2747e1e1a57208a5b5026faab6230084f8912b8.

--- toolbox/files.sh
+++ toolbox/files.sh
@@ -4,4 +4,4 @@
 # Creates a single compressed archive of files, reading paths from stdin.
 #
 
-xargs tar --hard-reference -hczf $1.tar.gz && echo $1.tar.gz && exit 0
+xargs tar -czf $1.tar.gz && echo $1.tar.gz && exit 0
