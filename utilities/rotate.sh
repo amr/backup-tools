e1af4508a238fb8677cdd1d2aa86a9ed10c73c4a
Ahmad Saif <ahmed.saif@egyptdc.com>
2010-07-06 13:27:18 +0300

    Modify rotate to rotate per project

--- utilities/rotate.sh
+++ utilities/rotate.sh
@@ -3,7 +3,7 @@
 #
 # Rotates backup items - to be used on the remote backup server
 #
-# Usage: ./rotate.sh /path/to/backup/root [days to keep]
+# Usage: ./rotate.sh /path/to/backup/root PROJECTNAME [days to keep]
 #
 
 # Default backup days to keep 
@@ -17,13 +17,18 @@ function fatal_error() {
 
 # Rotate entries in a given dir
 rotate () {
-        ls $1 | sort -rn | uniq | sed -e ''1,"$KEEP"d'' | while read backup; do test -n "$backup" && rm -rf $dir/$backup* || return 1; done;
+        ls $1 | sort -rn | uniq | sed -e ''1,"$KEEP"d'' | while read backup; do test -n "$backup" && rm -rf $dir/$backup || return 1; done;
 	return 0
 }
 
 # Keep
+if [ -n "$3" ]; then
+	KEEP="$3"
+fi
+
+# Project name
 if [ -n "$2" ]; then
-	KEEP="$2"
+	PROJECT_NAME="$2"
 fi
 
 # Validate
@@ -31,4 +36,4 @@ test -d "$1" || fatal_error "Invalid directory: $1"
 echo $KEEP | grep -q '^[0-9]\+$' || fatal_error "Invalid backup days to keep (numeric value expected): $KEEP"
 
 # Rotate!
-cd $1; find . -maxdepth 1 -type d | sed 1d | while read dir; do rotate $dir; done;
+cd $1; find $PROJECT_NAME -type d | head -n 1 | while read dir; do rotate $dir; done;
