98867be967b9691f1f69fd44285668b1341e38b6
Amr Mostafa <amr.mostafa@gmail.com>
2015-03-02 19:13:43 +0200

    Make mysql dump each table in a separate file, and dump SHOW MASTER STATUS

--- toolbox/mysql.sh
+++ toolbox/mysql.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 # Part of backup-tools
 #
 # Makes a backup copy of a MySQL database
@@ -20,7 +20,29 @@ fi
 # are correct
 mysqladmin $MYSQL_OPTS ping 2>&1 > /dev/null || exit 1
 
+MYSQL_CMD="mysql $MYSQL_OPTS"
+MYSQLDUMP_CMD="mysqldump $MYSQLDUMP_OPTS $MYSQL_OPTS"
+
 # Execute mysqldump on given databases
 for db in $PROJECT_MYSQL_DATABASES;
-	do mysqldump $MYSQLDUMP_OPTS $MYSQL_OPTS $db > $TMP_DIRECTORY/$PROJECT_NAME.DB-$db.sql && bzip2 $TMP_DIRECTORY/$PROJECT_NAME.DB-$db.sql && echo $TMP_DIRECTORY/$PROJECT_NAME.DB-$db.sql.bz2;
+	do
+	  mkdir -p $TMP_DIRECTORY/$PROJECT_NAME.DB-$db
+	  DUMP_TABLES=$(echo "SHOW TABLES" | $MYSQL_CMD $db | sed 1d | while read table
+	    do
+	      echo "SYSTEM $MYSQLDUMP_CMD $db $table > $TMP_DIRECTORY/$PROJECT_NAME.DB-$db/$table.sql;"
+      done)
+
+    STMTS="
+    FLUSH TABLES WITH READ LOCK;
+    SYSTEM echo \"SHOW MASTER STATUS\G\" | $MYSQL_CMD > $TMP_DIRECTORY/$PROJECT_NAME.DB-$db/00-master-status.txt;
+    $DUMP_TABLES;
+    UNLOCK TABLES;"
+
+    echo $STMTS | $MYSQL_CMD
+
+    for table in $TMP_DIRECTORY/$PROJECT_NAME.DB-$db/*.sql
+      do bzip2 $table
+    done
+
+    echo $TMP_DIRECTORY/$PROJECT_NAME.DB-$db
 done
